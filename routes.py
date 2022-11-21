93c995159e2b9f3e8f3bc1d7643639639d91dc59
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-11-21 13:33:17 +0100

    deploy

--- routes.py
+++ routes.py
@@ -22,4 +22,6 @@ def passed():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=8080)
+#    app.run(debug=True, port=5000)
