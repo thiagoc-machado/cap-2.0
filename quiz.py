a919d33368a3558c618958322d0ec31c638fa816
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-11-28 21:26:20 +0100

    changes

--- quiz.py
+++ quiz.py
@@ -22,20 +22,13 @@ def perg():
                 if txt in linha.split():
                     nome = linha
             else:
-                registro[0] = (linecache.getline("cap.txt", index)).replace(
-                        "\n", "")
-                registro[1] = (linecache.getline("cap.txt", index + 1)).replace(
-                        "\n", "")
-                registro[2] = (linecache.getline("cap.txt", index + 3)).replace(
-                        "\n", "")
-                registro[3] = (linecache.getline("cap.txt", index + 4)).replace(
-                        "\n", "")
-                registro[4] = (linecache.getline("cap.txt", index + 5)).replace(
-                        "\n", "")
-                registro[5] = (linecache.getline("Cap.txt", index + 6)).replace(
-                        "\n", "")
-                registro[6] = (linecache.getline("cap.txt", index + 7)).replace(
-                        "\n", "")
+                registro[0] = (linecache.getline("cap.txt", index)).replace("\n", "")
+                registro[1] = (linecache.getline("cap.txt", index + 1)).replace("\n", "")
+                registro[2] = (linecache.getline("cap.txt", index + 3)).replace("\n", "")
+                registro[3] = (linecache.getline("cap.txt", index + 4)).replace("\n", "")
+                registro[4] = (linecache.getline("cap.txt", index + 5)).replace("\n", "")
+                registro[5] = (linecache.getline("Cap.txt", index + 6)).replace("\n", "")
+                registro[6] = (linecache.getline("cap.txt", index + 7)).replace("\n", "")
                 dic.append(registro)
                 nome = ""
                 registro = {}
