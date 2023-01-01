401917907a7accc22119be959d089a66b1155f7d
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-01-01 13:46:35 +0100

    routes to quiz

--- app.py
+++ app.py
@@ -83,5 +83,13 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/', methods=['GET', 'POST'])
+def wtf_quiz():
+    p = perg()
+    return render_template('quiz.html',cod = p[0], perg = p[1], alt1 = p[2], alt2 = p[3], alt3 = p[4], alt4 = p[5], res = p[6], norma = p[7], ref = p[8], qtd = p[9])
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
